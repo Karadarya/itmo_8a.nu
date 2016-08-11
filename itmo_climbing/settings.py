6c8d8632207d2f01538eecd91494b1e0c44eccb9
Karadarya <Karadarya@gmail.com>
2016-08-12 02:09:32 +0400

    Update settings.py
    Changed settings to work with SQLite db

--- itmo_climbing/settings.py
+++ itmo_climbing/settings.py
@@ -79,12 +79,9 @@ WSGI_APPLICATION = 'itmo_climbing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'itmo_climbing',
-        'USER': 'itmo_admin',
-        'PASSWORD': 'itsmoreadmin',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
 }
 
