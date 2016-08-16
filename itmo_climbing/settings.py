cf821d4a9a18193b434526a31001999f2ced28b6
Karadarya <Karadarya@gmail.com>
2016-08-16 19:16:28 +0400

    set DEBUG False

--- itmo_climbing/settings.py
+++ itmo_climbing/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'szlhn(u102(st1-c(xn+mnt8czblzn#%kyool$+4d5tu(=fld1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
