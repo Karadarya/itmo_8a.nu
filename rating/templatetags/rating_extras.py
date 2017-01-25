9b6da156a3fa938f313a230bcb059e200f49ace0
Kara <karadarya@gmail.com>
2017-01-26 00:20:22 +0300

    front-end for unrated routes

--- rating/templatetags/rating_extras.py
+++ rating/templatetags/rating_extras.py
@@ -1,4 +1,5 @@
 from django import template
+from rating.models import Athlete_Route, Period
 
 register = template.Library()
 
@@ -10,3 +11,12 @@ def route_comments(comments, route):
 @register.filter(name='period_list')
 def period_list(routes, period):
     return routes.filter(period=period)
+
+@register.filter(name='rated')
+def rated(route):
+    #might need optimization
+    best = Athlete_Route.objects.filter(athlete=route.athlete).filter(period=Period.objects.get(current=True)).order_by('-remark__cost').order_by('-route__grade__cost')[:6]
+    if  ( route in best ):
+        return True
+    else:
+        return False
